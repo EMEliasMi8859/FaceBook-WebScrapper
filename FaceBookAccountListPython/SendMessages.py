707425978851d81dfd55ce9340ab80dd5804b367
Engineer Muhammad Elias Muhammadi <Muhammad.Elias.Muhammadi@gmail.com>
2023-10-06 19:33:40 +0430

    Add files via upload

---
+++ FaceBookAccountListPython/SendMessages.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Firefox()
+driver.get("https://www.facebook.com/3Leah")
+#the button element
+#button = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.ID, "Button_id")))
+button = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x1ypdohk xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x87ps6o x1lku1pv x1a2a7pz x9f619 x3nfvp2 xdt5ytf xl56j7k x1n2onr6 xh8yej3")))
+button.click()
+driver.quit()
